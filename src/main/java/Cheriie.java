6bcd9222f1f42f2e94c485d599d91219eb960862
ben <jwben03@gmail.com>
2026-01-29 13:28:22 +0800

    Add functionality to add a maximum of 100 items to a list and to display the list

--- src/main/java/Cheriie.java
+++ src/main/java/Cheriie.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class Cheriie {
     public static void main(String[] args) {
+        greeting();
+        // echo function goes here
+        echo();
+    }
+
+    public static void printHorizontalLinesBot() {
+        System.out.print("─".repeat(70).indent(3));
+    }
+
+    public static void greeting() {
         String logo = """
              ____ _
             / ___| |__   ___ _ __   (_) (_) ___
@@ -14,29 +24,35 @@ public class Cheriie {
         System.out.println("\thello there ! my name is Cheriie !");
         System.out.println("\twhat may i do for you today?");
         printHorizontalLinesBot();
-
-        // echo function goes here
-        echo();
-    }
-
-    public static void printHorizontalLinesBot() {
-        System.out.print("─".repeat(70).indent(3));
     }
 
     public static void echo() {
+        String[] listOfItems = new String[100];
+        int count = 0;
+        Scanner in = new Scanner(System.in);
         while (true) {
-            String line;
-            Scanner in = new Scanner(System.in);
-            line = in.nextLine();
+            String line = in.nextLine();
+            // if the user wants to quite straight away
             if (line.equalsIgnoreCase("bye")) {
                 printHorizontalLinesBot();
                 System.out.println("\tbye :) hope to hear from you again soon!");
                 printHorizontalLinesBot();
                 break;
             }
-            printHorizontalLinesBot();
-            System.out.print(line.indent(4));
-            printHorizontalLinesBot();
+            else if (line.equalsIgnoreCase("list")) {
+                printHorizontalLinesBot();
+                for (int i = 0; i < count; i++) {
+                    System.out.println("\t" + (i + 1) + ". " + listOfItems[i]);
+                }
+                printHorizontalLinesBot();
+            } else {
+                listOfItems[count] = line;
+                count++;
+                printHorizontalLinesBot();
+                System.out.println("\tadded: " + line);
+                printHorizontalLinesBot();
+            }
+
         }
     }
 }
