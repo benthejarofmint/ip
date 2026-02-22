4eb29b683b3e7083783052c08e5dcbc20e9f5940
ben <jwben03@gmail.com>
2026-02-22 20:32:28 +0800

    Add a Ui class to deal with all the interactions with the bot and the user

---
+++ src/main/java/cheriie/Ui.java
@@ -0,0 +1,35 @@
+package cheriie;
+
+import java.util.Scanner;
+
+public class Ui {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String getInput() {
+        return scanner.nextLine();
+    }
+
+    public static void showWelcomeMessage() {
+        Display.showWelcomeMessage();
+    }
+
+    public static void showEndMessage() {
+        Display.showEndMessage();
+    }
+
+    public static void printHorizontalLinesBot() {
+        Display.printHorizontalLinesBot();
+    }
+
+    public static void showHelpMessage() {
+        Display.showHelpMessage();
+    }
+
+    public static void response(String message) {
+        Display.response(message);
+    }
+
+    public static void print(String message) {
+        Display.print(message);
+    }
+}
